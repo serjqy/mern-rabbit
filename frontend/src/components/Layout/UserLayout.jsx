9775e40f5a825632c78bfa6c2a2d1aa26268aae9
serjay <serhiinote@gmail.com>
2025-12-29 12:43:52 +0000

    Home Page: New Arrivals & GenderCollection Sections

--- frontend/src/components/Layout/UserLayout.jsx
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Header from "../Common/Header";
 import Footer from "../Common/Footer";
+import { Outlet } from "react-router-dom";
 
 const UserLayout = () => {
   return (
     <>
       <Header />
-      {/* MainContent */}
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </>
   );
